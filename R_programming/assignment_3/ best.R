d36681f279bcbf2bacb1c349ecdad2a22d562e8e
Rich R <rich.robinson84@gmail.com>
2014-11-28 14:37:29 +0000

    untested but complete best.R
    Completed writing the code for best.R, but is currently untested.
    Testing commencing after this commit.

--- R_programming/assignment_3/ best.R
+++ R_programming/assignment_3/ best.R
@@ -10,13 +10,44 @@ best <- function(state, outcome) {
   
   ## Check that state and outcome are valid
   ## By splitting the data by state, then check to see if a matching state exists
-  data <- split(raw, raw.State)
-  snames <- names(date)
+  data.by.state <- split(raw, raw.State)
+  snames <- names(data.by.state)
+  bool <- lapply(snames, function(st) st == state)
+  ## converting bool from a list of lists of booleans, to a boolean
+  bool2 <- as.logical(bool)
   
+  if (sum(bool2) != 1){
+    ## state arguement is invalid
+    stop("invalid state")
+  }
+  ## else continue to check if outcome arguement is valid
+  ## to be valid it must be one of "heart attack", "heart failure" 
+  ## or "pheumonia".
+  ## Because there are only a few outcome, I will simply hard code
+  ## the corresponding column numbers to analyise.
   
+  col <- 0 ## The column to analyise
+  hos.name <- 2 ## The column to find the hospital name
   
+  ## checking outcome arguement and assigning corresponding column number to col
+  if (outcome == "heart attack"){
+    col <- 11
+  } else if (outcome == "heart failure"){
+    col <- 17
+  } else if (outcome == "pneumonia"){
+    col <- 23
+  } else {
+    stop("invalid outcome") ## outcome arguement is invalid
+  }
   
+  ## selecting only the data of the required state
+  data.of.state <- data.by.state$state
+  ## using complete.cases to remove entries with NA or NAN values
+  data.clean <- data.of.state[complete.cases(data.of.state[ , col]), ]
+  ## sorting the data first by 'col', then by 'Hospital Name'
+  sorted.data <- data.clean[order(data.clean[ , col], data.clean[ , hos.name]), ]
+      
   ## Return hospital name in that state with lowest 30-day death
   ## rate
-  
+  return(sorted.data[ 1, hos.name])
 }
