db3166c1b3642d17fa1124cdcff9918ed9b647b9
Rich R <rich.robinson84@gmail.com>
2015-01-09 18:21:37 +0000

    Work on loading all original data sets
    First session of coding.
    Focus on setting up structure code.
    Implemented code to load data into data tables for merging later.
    Spent 4 hours hacking this together. Revising how to do stuff in R and
    also updating R and all packages.

---
+++ Getting_&_Cleaning_Data/course_project/run_analysis.R
@@ -0,0 +1,44 @@
+## Libraries required
+library(dplyr)
+library(data.table)
+
+baseDataPath <- "./UCI HAR Dataset/"
+
+
+mergeDataSets <- function(){
+  #trainFiles <- c("train/X_train.txt", "train/subject_train.txt", "train/y_train.txt")
+  #testFiles <- c("test/X_test.txt", "test/subject_train.txt", test/y_test.txt"")
+  
+  features <- read.table("./UCI HAR Dataset/features.txt", sep=" ", stringsAsFactors=FALSE)
+  
+  
+  ## loading data from training set
+  X_train <- read.table(paste(baseDataPath, "train/X_traun.txt", sep=""), header=FALSE, dec=".", colClasses="numeric")
+  y_train <- read.table(paste(baseDataPath, "train/y_train.txt", sep=""), header=FALSE, colClasses="numeric")
+  subject_train <- read.table(paste(baseDataPath, "train/subject_train.txt", sep=""), header=FALSE, colClasses="numeric")
+  
+  ## Setting the column names training data tables
+  names(X_train) <- features$V2
+  names(y_train) <- "activity"
+  names(subject_train) <- "participant"
+  
+  ## merging the training data into a single data table
+  ## ... TODO
+  
+  ## loading data from test set
+  X_test <- read.table(paste(baseDataPath, "test/X_test.txt", sep=""), header=FALSE, dec=".", colClasses="numeric")
+  y_test <- read.table(paste(baseDataPath, "test/y_test.txt", sep=""), header=FALSE, colClasses="numeric")
+  subject_test <- read.table(paste(baseDataPath, "test/subject_test.txt", sep=""), header=FALSE, colClasses="numeric")
+  
+  ## Setting the column names for the test data tables
+  names(X_test) <- features$V2
+  names(y_test) <- "activity"
+  names(subject_test) <- "participant"
+  
+  ## Merging the test data into a single data table
+  ## ... TODO
+  
+  ## Making data set more readable by replacing numbers with descriptive text
+  ## ... TODO
+  
+}
